02c61f09dfde53831e722e1cf0275cd861fb9c1b
Matt Spitz <mattspitz@gmail.com>
2013-06-07 10:30:37 -0400

    Adding keybinding fix for BSD mktemp compatibility.

--- setup_completeme_key_binding.sh
+++ setup_completeme_key_binding.sh
@@ -1,1 +1,1 @@
-bind -x '"\C-t": TMPFILE=`mktemp` && env completeme $TMPFILE && test -e $TMPFILE && source $TMPFILE; rm -f $TMPFILE'
+bind -x '"\C-t": TMPFILE=`mktemp 2> /dev/null || mktemp -t completeme 2> /dev/null` && env completeme $TMPFILE && test -e $TMPFILE && source $TMPFILE; rm -f $TMPFILE'
